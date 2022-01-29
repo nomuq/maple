1d16fa38db1b7e2c540af1407c5f8cb28a0a27b8
Satish Babariya <satish.babariya@gmail.com>
2022-01-29 20:23:16 +0530

    add firebase config

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCGLrHNkbeFlKk6gwueR9SCV3sCrOHXZgM",
+  authDomain: "maple-issue-tracker.firebaseapp.com",
+  projectId: "maple-issue-tracker",
+  storageBucket: "maple-issue-tracker.appspot.com",
+  messagingSenderId: "34137832421",
+  appId: "1:34137832421:web:71ca513a3bd3eef6a34d0c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
