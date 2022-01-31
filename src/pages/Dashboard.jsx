65c443e922985524a79e96a15cc381ae7c33ef96
Satish Babariya <satish.babariya@gmail.com>
2022-01-31 23:00:11 +0530

    add list projects

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,19 @@
 import { getAuth } from "firebase/auth";
 import React from "react";
-import { Avatar, Button, Modal, PageLoader } from "../components";
+import {
+  Avatar,
+  Button,
+  Modal,
+  PageLoader,
+  ProjectAvatar,
+} from "../components";
 import { ProjectService } from "../services/ProjectService";
 import styled from "styled-components";
 import { color, font, mixin, sizes } from "../styles/styles";
 import ProjectCreate from "./ProjectCreate";
 import { UserService } from "../services/UserService";
 import { Link } from "react-router-dom";
+import DynamicTable from "@atlaskit/dynamic-table";
 
 export default function Dashboard() {
   const auth = getAuth();
@@ -80,121 +87,149 @@ export default function Dashboard() {
           </Button>
         </Header>
 
-        <ProjectsList>
-          {projects &&
-            projects.map((project) => (
-              <ProjectLink
-                to={`/projects/${project.id}`}
-                key={`/projects/${project.id}`}
-              >
-                <Project key={project.id}>
-                  <ProjectTitle>{project.name}</ProjectTitle>
-                  <Bottom>
-                    <div>
-                      <ProjectCategory>{project.category}</ProjectCategory>
-                    </div>
-                    <Assignees>
-                      {[...project.collaborators, project.createdBy].map(
-                        (collaboratorID) => {
-                          const collaborator = users.find(
-                            (user) => user.uid === collaboratorID
-                          );
-                          return (
-                            <AssigneeAvatar
-                              key={collaborator.uid}
-                              size={24}
-                              avatarUrl={collaborator.photoURL}
-                              name={collaborator.displayName}
-                            />
-                          );
-                        }
-                      )}
-                    </Assignees>
-                  </Bottom>
-                </Project>
-              </ProjectLink>
-            ))}
-        </ProjectsList>
+        <Project>
+          {projects && (
+            <DynamicTable
+              head={{
+                cells: [
+                  {
+                    key: "name",
+                    content: "Name",
+                    isSortable: true,
+                  },
+                  {
+                    key: "type",
+                    content: "Type",
+                    isSortable: true,
+                  },
+                  {
+                    key: "createdBy",
+                    content: "Owner",
+                    isSortable: true,
+                  },
+                  {
+                    key: "collaborators",
+                    content: "Collaborators",
+                    isSortable: false,
+                  },
+
+                  {
+                    key: "category",
+                    content: "Category",
+                    isSortable: true,
+                  },
+                ],
+              }}
+              rows={projects.map((project) => {
+                return {
+                  key: project.id,
+                  cells: [
+                    {
+                      key: "name",
+                      content: (
+                        <ProjectTitle>
+                          <ProjectAvatar size={24} />
+                          <ProjectLink to={`/projects/${project.id}`}>
+                            {project.name}
+                          </ProjectLink>
+                        </ProjectTitle>
+                      ),
+                    },
+                    {
+                      key: "type",
+                      content: <div>{project.type}</div>,
+                    },
+                    {
+                      key: "createdBy",
+                      content: (
+                        <div>
+                          {
+                            users.filter(
+                              (user) => user.uid == project.createdBy
+                            )[0].displayName
+                          }
+                        </div>
+                      ),
+                    },
+                    {
+                      key: "collaborators",
+                      content: (
+                        <Assignees>
+                          {[...project.collaborators, project.createdBy].map(
+                            (collaboratorID) => {
+                              const collaborator = users.find(
+                                (user) => user.uid === collaboratorID
+                              );
+                              return (
+                                <AssigneeAvatar
+                                  key={collaborator.uid}
+                                  size={24}
+                                  avatarUrl={collaborator.photoURL}
+                                  name={collaborator.displayName}
+                                />
+                              );
+                            }
+                          )}
+                        </Assignees>
+                      ),
+                    },
+                    {
+                      key: "category",
+                      content: <div>{project.category}</div>,
+                    },
+                  ],
+                };
+              })}
+            />
+          )}
+        </Project>
       </Projects>
     </>
   );
 }
 
-export const Projects = styled.div`
+const Projects = styled.div`
   padding: 25px;
 `;
 
-export const Header = styled.div`
+const Header = styled.div`
   margin-top: 6px;
   display: flex;
   justify-content: space-between;
+  padding-bottom: 25px;
 `;
 
-export const Title = styled.div`
+const Title = styled.div`
   ${font.size(24)}
   ${font.medium}
 `;
 
-const ProjectsList = styled.div`
+const ProjectTitle = styled.p`
   display: flex;
-  flex-direction: column;
-  margin-top: 25px;
+  flex-direction: row;
 `;
 
-export const ProjectTitle = styled.p`
-  padding-bottom: 11px;
-  ${font.size(18)}
-  @media (max-width: 1100px) {
-    ${font.size(17.5)}
-  }
-`;
-
-export const ProjectLink = styled(Link)`
+const ProjectLink = styled(Link)`
   display: block;
   margin-bottom: 5px;
+  text-decoration: underline;
+  color: ${color.primary};
+  padding-left: 5px;
 `;
 
-export const Project = styled.div`
-  padding: 10px;
-  border-radius: 3px;
-  background: #fff;
-  box-shadow: 0px 1px 2px 0px rgba(9, 30, 66, 0.25);
-  transition: background 0.1s;
-  ${mixin.clickable}
-  @media (max-width: 1100px) {
-    padding: 10px 8px;
-  }
-  &:hover {
-    background: ${color.backgroundLight};
-  }
-  ${(props) =>
-    props.isBeingDragged &&
-    css`
-      transform: rotate(3deg);
-      box-shadow: 5px 10px 30px 0px rgba(9, 30, 66, 0.15);
-    `}
+const Project = styled.div``;
+
+const Divider = styled.div`
+  margin-top: 22px;
+  border-top: 1px solid ${color.borderLightest};
 `;
 
-const ProjectCategory = styled.p`
-  ${font.size(12)}
-  ${font.medium}
-  color: ${color.textLight};
-  margin-bottom: 0;
-`;
-
-export const Bottom = styled.div`
+const Assignees = styled.div`
   display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-export const Assignees = styled.div`
-  display: flex;
-  flex-direction: row-reverse;
   margin-left: 2px;
 `;
 
-export const AssigneeAvatar = styled(Avatar)`
+const AssigneeAvatar = styled(Avatar)`
   margin-left: -2px;
   box-shadow: 0 0 0 2px #fff;
 `;
